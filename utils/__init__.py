47a82d23a4e0b9d573a95abe9ded5aa02a6cffca
kkosty <kosgaponenko@gmail.com>
2026-03-22 23:32:56 +0200

    Add referral discount functions to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,5 @@ from .utils import (
     get_cart, cart_empty, clear_cart, cart_total,
     add_to_cart, cart_items, format_cart,
     recommend_size, is_admin, ref_link, build_receipt,
+    get_referral_discount, apply_discount,
 )
